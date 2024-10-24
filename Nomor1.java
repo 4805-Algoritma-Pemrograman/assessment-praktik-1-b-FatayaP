c04c21ccc9b51620751efdff9f678798e378f900
FatayaP <110081846+FatayaP@users.noreply.github.com>
2024-10-24 09:23:03 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,6 +2,22 @@
 
 public class Nomor1{
     public static void main(String[] args){
-      
+      Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan total penjualan salesman: ");
+        double totalPenjualan = input.nextDouble();
+
+        double komisi;
+
+        if (totalPenjualan <= 500000) {
+            komisi = totalPenjualan * 0.10; 
+        } else {
+            double komisi500k = 500000 * 0.10; 
+            double komisiSisanya = (totalPenjualan - 500000) * 0.15; 
+            komisi = komisi500k + komisiSisanya;
+        }
+
+        System.out.println("Total penjualan salesman: " + totalPenjualan);
+        System.out.println("Komisi yang didapatkan = " + komisi);
     }
 }
