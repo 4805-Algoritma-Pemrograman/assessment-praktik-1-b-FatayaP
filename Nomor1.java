3c53ad6c1f740e542bf8a23d3adaed5df6c1b5ea
FatayaP <110081846+FatayaP@users.noreply.github.com>
2024-10-24 10:24:07 +0700

    Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,17 +3,22 @@
 public class Nomor1{
     public static void main(String[] args){
       Scanner input = new Scanner(System.in);
-        
+
+        System.out.print("Masukkan total penjualan salesman: ");
         double totalPenjualan = input.nextDouble();
+
         double komisi;
 
         if (totalPenjualan <= 500000) {
             komisi = totalPenjualan * 0.10; 
         } else {
-            komisi = 500000 * 0.10 + (totalPenjualan - 500000) * 0.15;
+            double komisi500k = 500000 * 0.10; 
+            double komisiSisanya = (totalPenjualan - 500000) * 0.15; 
+            komisi = komisi500k + komisiSisanya;
         }
 
-        System.out.println(komisi);
+        System.out.println("Total penjualan salesman: " + totalPenjualan);
+        System.out.println("Komisi yang didapatkan = " + komisi);
         
     }  
     
